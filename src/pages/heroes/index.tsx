2d0d5622d5128a8633d6cad87b3b324010a47a2d
Kiki <zoeluoluo05673@gmail.com>
2023-04-15 21:59:21 +0800

    feat: main page data fetching

--- src/pages/heroes/index.tsx
+++ src/pages/heroes/index.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+
 const HeroMainPage = () => {
+    interface HeroDate {
+        id: string, name: string, image: string
+    }
+    const [data, setData] = useState<HeroDate[] | null>(null)
+    const [isLoading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://hahow-recruit.herokuapp.com/heroes')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
     return (
-        <div>This is main page</div>
+        <>
+            {
+                data.map((d) => {
+                    return <>
+                        <div>{d.id}</div>
+                        <div>{d.name}</div>
+                        <img src={d.image} alt="hero image" />
+                    </>
+                })
+            }
+
+        </>
     )
-  }
-  
-  export default HeroMainPage
+}
+
+export default HeroMainPage
