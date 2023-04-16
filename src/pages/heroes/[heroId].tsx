2fb01aba69e17a9473995ffaf33fab9d4411ff7b
Kiki <zoeluoluo05673@gmail.com>
2023-04-16 16:39:20 +0800

    feat: add PATCH data flow

--- src/pages/heroes/[heroId].tsx
+++ src/pages/heroes/[heroId].tsx
@@ -13,6 +13,7 @@ const HeroDetail = () => {
     const router = useRouter()
     const { heroId } = router.query
     const [data, setData] = useState<HeroDatil | null>(null)
+    const [remainCount, setRemainCount] = useState<number | null>(null)
     const [availableCount, setAvailableCount] = useState<number | null>(null)
     const [isLoading, setLoading] = useState(false)
     useEffect(() => {
@@ -24,11 +25,21 @@ const HeroDetail = () => {
                 setData(data)
                 setLoading(false)
                 const sumData = Object.values(data).reduce((acc, cur) => acc + cur);
+                console.log(sumData)
                 setAvailableCount(sumData)
             })
-    }, [])
+    }, [heroId])
+    const checkRemain = () => {
+        console.log(availableCount)
+        if (!data || (!availableCount && availableCount !== 0)) {
+            return
+        }
+        const nowSumData = Object.values(data).reduce((acc, cur) => acc + cur);
+        setRemainCount(availableCount - nowSumData)
+    }
     useEffect(() => {
         console.log(data)
+        checkRemain();
     }, [data])
     if (isLoading) return <p>Loading...</p>
     if (!data) return <p>No profile data</p>
@@ -39,25 +50,38 @@ const HeroDetail = () => {
             [`${key}`]: data[key] + count,
         })
     }
-    const checkRemain = () => {
-        const nowSumData = Object.values(data).reduce((acc, cur) => acc + cur);
-        console.log(nowSumData + '   ' + availableCount)
-        if (availableCount && (availableCount - nowSumData < 1)) {
-            return true
+    const onClickSendData = async () => {
+        if((!remainCount || remainCount > 0) && remainCount !== 0){
+            alert('剩餘點數不得為 0 以上')
+            return
         }
-        return false
+        setLoading(true)
+        console.log(JSON.stringify(data))
+        fetch(`https://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res)=>{
+            console.log(res)
+            setLoading(false)
+        })
+
     }
     return (
         <>
             {Object.keys(data).map((key) => (
                 <>
                     <div>
-                        <button disabled={checkRemain()} onClick={() => { onClickButtonAction(key, 1) }}>+</button>
+                        <button disabled={remainCount < 1} onClick={() => { onClickButtonAction(key, 1) }}>+</button>
                         <div>{`${key} : ${data[key]}`}</div>
                         <button disabled={data[key] < 1} onClick={() => { onClickButtonAction(key, -1) }}>-</button>
                     </div>
                 </>
             ))}
+            <div>Remain Count : {remainCount}</div>
+            <button onClick={onClickSendData}>SEND</button>
             <Link href={`/heroes`}>Beck To HOME page</Link>
         </>
     )
